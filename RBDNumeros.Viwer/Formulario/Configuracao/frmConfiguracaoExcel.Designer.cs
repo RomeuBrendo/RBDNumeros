06dc2e71b157dd7a3f8627518723a6c0fed7b1d5
Romeu Brendo <romeu.gomes@lbc.com.br>
2020-07-02 01:11:26 -0300

    commit parcial tela config

---
+++ RBDNumeros.Viwer/Formulario/Configuracao/frmConfiguracaoExcel.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace RBDNumeros.Viwer.Formulario.Configuracao
+{
+    partial class frmConfiguracaoExcel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtNumeroTicket = new System.Windows.Forms.TextBox();
+            this.txtNomeCliente = new System.Windows.Forms.TextBox();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.txtDataAbertura = new System.Windows.Forms.TextBox();
+            this.txtDataResolvido = new System.Windows.Forms.TextBox();
+            this.txtTecnico = new System.Windows.Forms.TextBox();
+            this.txtCarteira = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número do Ticket:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome Cliente:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Categoria:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Data Abertura Ticket:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Data Resolvido";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 270);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Tecnico";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 307);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Carteira";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(53, 20);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(663, 24);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Preencha  coluna do CSV que se encontra cada campo p/ importação";
+            // 
+            // txtNumeroTicket
+            // 
+            this.txtNumeroTicket.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNumeroTicket.Location = new System.Drawing.Point(149, 82);
+            this.txtNumeroTicket.Name = "txtNumeroTicket";
+            this.txtNumeroTicket.Size = new System.Drawing.Size(100, 20);
+            this.txtNumeroTicket.TabIndex = 8;
+            // 
+            // txtNomeCliente
+            // 
+            this.txtNomeCliente.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNomeCliente.Location = new System.Drawing.Point(149, 115);
+            this.txtNomeCliente.Name = "txtNomeCliente";
+            this.txtNomeCliente.Size = new System.Drawing.Size(100, 20);
+            this.txtNomeCliente.TabIndex = 9;
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtCategoria.Location = new System.Drawing.Point(149, 152);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(100, 20);
+            this.txtCategoria.TabIndex = 10;
+            // 
+            // txtDataAbertura
+            // 
+            this.txtDataAbertura.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtDataAbertura.Location = new System.Drawing.Point(149, 189);
+            this.txtDataAbertura.Name = "txtDataAbertura";
+            this.txtDataAbertura.Size = new System.Drawing.Size(100, 20);
+            this.txtDataAbertura.TabIndex = 11;
+            // 
+            // txtDataResolvido
+            // 
+            this.txtDataResolvido.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtDataResolvido.Location = new System.Drawing.Point(149, 226);
+            this.txtDataResolvido.Name = "txtDataResolvido";
+            this.txtDataResolvido.Size = new System.Drawing.Size(100, 20);
+            this.txtDataResolvido.TabIndex = 12;
+            // 
+            // txtTecnico
+            // 
+            this.txtTecnico.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtTecnico.Location = new System.Drawing.Point(149, 263);
+            this.txtTecnico.Name = "txtTecnico";
+            this.txtTecnico.Size = new System.Drawing.Size(100, 20);
+            this.txtTecnico.TabIndex = 13;
+            // 
+            // txtCarteira
+            // 
+            this.txtCarteira.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtCarteira.Location = new System.Drawing.Point(149, 300);
+            this.txtCarteira.Name = "txtCarteira";
+            this.txtCarteira.Size = new System.Drawing.Size(100, 20);
+            this.txtCarteira.TabIndex = 14;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(293, 375);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 15;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(400, 375);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 16;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // frmConfiguracaoExcel
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(768, 442);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtCarteira);
+            this.Controls.Add(this.txtTecnico);
+            this.Controls.Add(this.txtDataResolvido);
+            this.Controls.Add(this.txtDataAbertura);
+            this.Controls.Add(this.txtCategoria);
+            this.Controls.Add(this.txtNomeCliente);
+            this.Controls.Add(this.txtNumeroTicket);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmConfiguracaoExcel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Configuração CSV";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtNumeroTicket;
+        private System.Windows.Forms.TextBox txtNomeCliente;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.TextBox txtDataAbertura;
+        private System.Windows.Forms.TextBox txtDataResolvido;
+        private System.Windows.Forms.TextBox txtTecnico;
+        private System.Windows.Forms.TextBox txtCarteira;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
---
+++ RBDNumeros.Viwer/Formulario/Configuracao/frmConfiguracaoExcel.cs
@@ -0,0 +1,27 @@
+﻿using RBDNumeros.Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RBDNumeros.Viwer.Formulario.Configuracao
+{
+    public partial class frmConfiguracaoExcel : Form
+    {
+        public frmConfiguracaoExcel()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            AdicionarConfiguracaoPlanilhaRequest adicionarConfiguracaoPlanilhaRequest = new AdicionarConfiguracaoPlanilhaRequest();
+        
+        }
+    }
+}
