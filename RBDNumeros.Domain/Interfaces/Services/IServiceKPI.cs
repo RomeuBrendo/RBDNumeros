6509c4904471606c5ab6f9f59d233531d6e7ae58
Romeu Brendo <romeu.gomes@lbc.com.br>
2020-08-02 11:13:35 -0300

    Correção parametro

--- RBDNumeros.Domain/Interfaces/Services/IServiceKPI.cs
+++ RBDNumeros.Domain/Interfaces/Services/IServiceKPI.cs
@@ -1,11 +1,11 @@
-﻿using RBDNumeros.Domain.Commands.KPI;
-using RBDNumeros.Domain.Entities.KPI;
+﻿using RBDNumeros.Domain.Entities.KPI;
 using RBDNumeros.Domain.Interfaces.Services.Base;
+using System;
 
 namespace RBDNumeros.Domain.Interfaces.Services
 {
     public interface IServiceKPI : IServiceBase
     {
-        ChamadosPorCarteira ChamadosPorCarteira(ChamadosPorCarteiraRequest request);
+        ChamadosPorCarteira ChamadosPorCarteira(DateTime dataInicio, DateTime dataFim);
     }
 }
